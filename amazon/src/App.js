54ef351e3e7ff95e189e5b78c5a90c1f1cee9190
nachiket <gujalwar6@gmail.com>
2021-03-23 00:35:35 +0530

    new project

---
+++ amazon/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import './Upper.css';
+import Upper from './components/Upper';
+import 'fomantic-ui-css/semantic.css';
+import './assets/fomantic/dist/semantic.css';
+import { Container, Header } from 'semantic-ui-react';
+
+
+function App() {
+  return (
+    <div>
+    
+
+      <Upper/>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon/src/components/Upper.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import 'fomantic-ui-css/semantic.css';
+import '../assets/fomantic/dist/semantic.css';
+import logo from '../images/download.png'
+
+function Upper() {
+    return (
+        <div class="ui fluid container" className="c1">
+            <div class="ui grid">
+                <div class="row" className="row">
+
+
+
+
+                    <div class="two wide column" className="logo">
+                        <img class="ui small image" src={logo} />
+                    </div>
+
+
+
+                    <div class="two wide column" className="add">
+                        Hello
+                        <p><span className="textbold">Select Your Address</span></p>
+
+
+                    </div>
+                    <div class="seven wide column">
+                        <div class="ui action input">
+
+                            <div class="ui dropdown" className="dropdown_1">
+                                <div >All</div>
+                                <i class="dropdown icon"></i>
+                            </div>
+
+
+                            <input size="65" type="text" />
+
+                            <button class="ui icon button" className="searchbutton">
+                                <i class="search icon"></i>
+                            </button>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+                    <div class="one wide column" className="flag">
+                        <div class="ui compact selection dropdown">
+                            <i class="dropdown icon"></i>
+                            <div class="text">Compact</div>
+                            <div class="menu">
+                                <div class="item">A</div>
+                                <div class="item">B</div>
+                                <div class="item">C</div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="two wide column" className="sign">
+                        Hello,Sign in
+                        <p>Account & Lists</p>
+                    </div>
+
+                    <div class="one wide column" className="return">
+                        <a href="#">
+                            Returns
+                        <p>
+                                & Order
+                        </p>
+                        </a>
+                    </div>
+                    <div class="one wide column" className="cart">
+
+                        <i class="shopping cart icon"></i>
+                        <p>Cart</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Upper
+
+
