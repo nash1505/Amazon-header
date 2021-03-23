b23630023de7a74684d05304736c8b75025bf8ee
nachiket <gujalwar6@gmail.com>
2021-03-23 20:09:18 +0530

    fix

--- amazon/src/components/Lower.js
+++ amazon/src/components/Lower.js
@@ -15,25 +15,44 @@ function Lower() {
                 <div class="row">
                     <div class="four wide column">
                         <div class="ui list">
-                            <div class="item">
-                                1
-  </div>
-                            <div class="item">
-                                2
-  </div>
-                            <div class="item">
-                                3
-  </div>
+                            <div class="item">Get To Know Us</div>
+                            <div class="item">About Us</div>
+                            <div class="item">Careers</div>
+                            <div class="item">Press Releases</div>
+                            <div class="item">Amazon Cares</div>
+                            <div class="item">Gift a Smile</div>
                         </div>
                     </div>
                     <div class="four wide column">
-
+                    <div class="ui list">
+                            <div class="item">Connect With Us</div>
+                            <div class="item">Facebook</div>
+                            <div class="item">Twitter</div>
+                            <div class="item">Instagram</div>
+                        </div>
                     </div>
                     <div class="four wide column">
-
+                    <div class="ui list">
+                            <div class="item">Make Money with Us</div>
+                            <div class="item">Sell on Amazon</div>
+                            <div class="item">Sell Under Amazon Accelerator</div>
+                            <div class="item">Amazon Global Selling</div>
+                            <div class="item">Become an Affilate</div>
+                            <div class="item">Fulfillment by Amazon</div>
+                            <div class="item">Advertise YOur Products</div>
+                            <div class="item">Amazon pay on Merchants</div>
+                            <div class="item">See More Make Money With Us</div>
+                        </div>
                     </div>
                     <div class="four wide column">
-
+                    <div class="ui list">
+                            <div class="item">Let us Help you</div>
+                            <div class="item">COVID-19 and Amazon</div>
+                            <div class="item">Your Account</div>
+                            <div class="item">Returns Center</div>
+                            <div class="item">100% Purchase Protection</div>
+                            <div class="item">Amazon App Download</div>
+                        </div>
                     </div>
 
                 </div>
