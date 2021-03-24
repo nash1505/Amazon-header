44dbd283427c082779f07150519195805399d2bf
nachiket <gujalwar6@gmail.com>
2021-03-24 11:08:22 +0530

    footer continued

--- amazon/src/components/Lower.js
+++ amazon/src/components/Lower.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'fomantic-ui-css/semantic.css';
 import '../assets/fomantic/dist/semantic.css';
-
+import logo from '../images/download.png'
 
 
 function Lower() {
@@ -12,50 +12,72 @@ function Lower() {
                     <p>Back to Top</p>
                 </div>
 
-                <div class="row">
+                <div class="row footer">
                     <div class="four wide column">
                         <div class="ui list">
-                            <div class="item">Get To Know Us</div>
-                            <div class="item">About Us</div>
-                            <div class="item">Careers</div>
-                            <div class="item">Press Releases</div>
-                            <div class="item">Amazon Cares</div>
-                            <div class="item">Gift a Smile</div>
+                            <div class="item"><a href="#">Get To Know Us</a></div>
+                            <div class="item"><a href="#">About Us</a></div>
+                            <div class="item"><a href="#">Careers</a></div>
+                            <div class="item"><a href="#">Press Releases</a></div>
+                            <div class="item"><a href="#">Amazon Cares</a></div>
+                            <div class="item"><a href=""><a href="#">Gift a Smile</a></a></div>
                         </div>
                     </div>
                     <div class="four wide column">
-                    <div class="ui list">
-                            <div class="item">Connect With Us</div>
-                            <div class="item">Facebook</div>
-                            <div class="item">Twitter</div>
-                            <div class="item">Instagram</div>
+                        <div class="ui list">
+                            <div class="item"><a href="#">Connect With Us</a></div>
+                            <div class="item"><a href="#">Facebook</a></div>
+                            <div class="item"><a href="#">Twitter</a></div>
+                            <div class="item"><a href="#">Instagram</a></div>
                         </div>
                     </div>
                     <div class="four wide column">
-                    <div class="ui list">
-                            <div class="item">Make Money with Us</div>
-                            <div class="item">Sell on Amazon</div>
-                            <div class="item">Sell Under Amazon Accelerator</div>
-                            <div class="item">Amazon Global Selling</div>
-                            <div class="item">Become an Affilate</div>
-                            <div class="item">Fulfillment by Amazon</div>
-                            <div class="item">Advertise YOur Products</div>
-                            <div class="item">Amazon pay on Merchants</div>
-                            <div class="item">See More Make Money With Us</div>
+                        <div class="ui list">
+                            <div class="item"><a href="#">Make Money with Us</a></div>
+                            <div class="item"><a href="#">Sell on Amazon</a></div>
+                            <div class="item"><a href="#">Sell Under Amazon Accelerator</a></div>
+                            <div class="item"><a href="#">Amazon Global Selling</a></div>
+                            <div class="item"><a href="#">Become an Affilate</a></div>
+                            <div class="item"><a href="#">Fulfillment by Amazon</a></div>
+                            <div class="item"><a href="#">Advertise Your Products</a></div>
+                            <div class="item"><a href="#">Amazon pay on Merchants</a></div>
+                            <div class="item"><a href="#">See More Make Money With Us</a></div>
                         </div>
                     </div>
                     <div class="four wide column">
-                    <div class="ui list">
-                            <div class="item">Let us Help you</div>
-                            <div class="item">COVID-19 and Amazon</div>
-                            <div class="item">Your Account</div>
-                            <div class="item">Returns Center</div>
-                            <div class="item">100% Purchase Protection</div>
-                            <div class="item">Amazon App Download</div>
+                        <div class="ui list">
+                            <div class="item"><a href="#">Let us Help you</a></div>
+                            <div class="item"><a href="#">COVID-19 and Amazon</a></div>
+                            <div class="item"><a href="#">Your Account</a></div>
+                            <div class="item"><a href="#">Returns Center</a></div>
+                            <div class="item"><a href="#">100% Purchase Protection</a></div>
+                            <div class="item"><a href="#">Amazon App Download</a></div>
+                        </div>
+                    </div>
+                    <div class="ui divider"></div>
+                    <div class="ui two column centered grid footer-continue">
+                        <div class="four column centered row">
+                            <div class="column"> <img class="ui small image" src={logo} alt="logo" /></div>
+                            <div class="column" >
+                           
+                                <select class="ui selection dropdown" className="d1">
+                                
+                                    <option value=""> <i class="globe icon"></i>English</option>
+                                    <option value="0">Hindi</option>
+                                    <option value="1">French</option>
+                                    <option value="2">Italian</option>
+                                    <option value="3">Arabic</option>
+                                    
+                                </select>
+                            </div>
                         </div>
                     </div>
 
+
+
                 </div>
+
+
 
             </div>
         </div>
